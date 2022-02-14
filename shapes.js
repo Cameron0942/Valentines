9d919f9c5b651ce0ce0fcabb016d0149e266f6fc
Cameron0942 <burnsc0942@gmail.com>
2022-02-14 15:53:19 -0800

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -56,30 +56,30 @@ function moveSquare() {
    
     square1.style.transform = "translate("+(originX - 25)+"px, "+(m + posY + originY - 25)+"px)";
     square1.style.zIndex = "1";
-    message1.style.zIndex = "10";
-    message2.style.zIndex = "10";
-    message3.style.zIndex = "10";
-    message4.style.zIndex = "10";
-    message5.style.zIndex = "10";
-    conc.style.zIndex = "10";
+    message1.style.zIndex = "2";
+    message2.style.zIndex = "2";
+    message3.style.zIndex = "2";
+    message4.style.zIndex = "2";
+    message5.style.zIndex = "2";
+    conc.style.zIndex = "2";
 
     square2.style.transform = "translate("+(originX - 50)+"px, "+(posY + originY - 50)+"px)";
     square2.style.zIndex = "0";
-    message1.style.zIndex = "10";
-    message2.style.zIndex = "10";
-    message3.style.zIndex = "10";
-    message4.style.zIndex = "10";
-    message5.style.zIndex = "10";
-    conc.style.zIndex = "10";
+    message1.style.zIndex = "2";
+    message2.style.zIndex = "2";
+    message3.style.zIndex = "2";
+    message4.style.zIndex = "2";
+    message5.style.zIndex = "2";
+    conc.style.zIndex = "2";
 
     square3.style.transform = "translate("+(originX - 75)+"px, "+((-1 * m) + posY + originY - 75)+"px)";
     square3.style.zIndex = "-1";
-    message1.style.zIndex = "10";
-    message2.style.zIndex = "10";
-    message3.style.zIndex = "10";
-    message4.style.zIndex = "10";
-    message5.style.zIndex = "10";
-    conc.style.zIndex = "10";
+    message1.style.zIndex = "2";
+    message2.style.zIndex = "2";
+    message3.style.zIndex = "2";
+    message4.style.zIndex = "2";
+    message5.style.zIndex = "2";
+    conc.style.zIndex = "2";
     
 }
 
