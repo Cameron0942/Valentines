51008d807827c63e37ccda7feecfb7d137677bb8
Cameron0942 <burnsc0942@gmail.com>
2022-02-14 15:42:06 -0800

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -2,6 +2,12 @@ window.onload = function() {
     let square1 = document.querySelector(".square1");
     let square2 = document.querySelector(".square2");
     let square3 = document.querySelector(".square3");
+    let message1 = document.querySelector("#fact1");
+    let message2 = document.querySelector("#fact2");
+    let message3 = document.querySelector("#fact3");
+    let message4 = document.querySelector("#fact4");
+    let message5 = document.querySelector("#fact5");
+    let conc = document.querySelector("#conc");
 
     let posX = window.scrollX;
     let posY = window.scrollY;
@@ -16,12 +22,30 @@ window.onload = function() {
    
     square1.style.transform = "translate("+(originX - 25)+"px, "+(m + posY + originY - 25)+"px)";
     square1.style.zIndex = "1";
+    message1.style.zIndex = "10";
+    message2.style.zIndex = "10";
+    message3.style.zIndex = "10";
+    message4.style.zIndex = "10";
+    message5.style.zIndex = "10";
+    conc.style.zIndex = "10";
 
     square2.style.transform = "translate("+(originX - 50)+"px, "+(posY + originY - 50)+"px)";
     square2.style.zIndex = "0";
+    message1.style.zIndex = "10";
+    message2.style.zIndex = "10";
+    message3.style.zIndex = "10";
+    message4.style.zIndex = "10";
+    message5.style.zIndex = "10";
+    conc.style.zIndex = "10";
 
     square3.style.transform = "translate("+(originX - 75)+"px, "+((-1 * m) + posY + originY - 75)+"px)";
     square3.style.zIndex = "-1";
+    message1.style.zIndex = "10";
+    message2.style.zIndex = "10";
+    message3.style.zIndex = "10";
+    message4.style.zIndex = "10";
+    message5.style.zIndex = "10";
+    conc.style.zIndex = "10";
 }
 
 function test() {
