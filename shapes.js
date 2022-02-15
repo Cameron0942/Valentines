27870cbaf633894b3c527d52f0ec5884414467c8
Cameron0942 <burnsc0942@gmail.com>
2022-02-14 16:01:34 -0800

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -55,7 +55,7 @@ function moveSquare() {
 
    
     square1.style.transform = "translate("+(originX - 25)+"px, "+(m + posY + originY - 25)+"px)";
-    square1.style.zIndex = "1";
+    //square1.style.zIndex = "1";
     message1.style.zIndex = "2";
     message2.style.zIndex = "2";
     message3.style.zIndex = "2";
@@ -64,7 +64,7 @@ function moveSquare() {
     conc.style.zIndex = "2";
 
     square2.style.transform = "translate("+(originX - 50)+"px, "+(posY + originY - 50)+"px)";
-    square2.style.zIndex = "0";
+    //square2.style.zIndex = "0";
     message1.style.zIndex = "2";
     message2.style.zIndex = "2";
     message3.style.zIndex = "2";
@@ -73,7 +73,7 @@ function moveSquare() {
     conc.style.zIndex = "2";
 
     square3.style.transform = "translate("+(originX - 75)+"px, "+((-1 * m) + posY + originY - 75)+"px)";
-    square3.style.zIndex = "-1";
+    //square3.style.zIndex = "-1";
     message1.style.zIndex = "2";
     message2.style.zIndex = "2";
     message3.style.zIndex = "2";
